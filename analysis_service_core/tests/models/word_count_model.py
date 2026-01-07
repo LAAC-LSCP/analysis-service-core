3490c5a02a81620b1e0d2708b38c9eaac2c720d9
Lawrence Borst <laurens.s.borst@gmail.com>
2026-01-08 00:24:44 +0100

    feat: mvp
    added ModelPlugin class that lets you create machine learning models
    that are naturally integrated into the ecosystem

---
+++ analysis_service_core/tests/models/word_count_model.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+from typing import List, Optional
+
+from analysis_service_core.src.config import Config
+from analysis_service_core.src.model import ModelPlugin
+from analysis_service_core.src.redis.pubsub import PubSub
+
+
+class WordCountModel(ModelPlugin):
+    """
+    A dummy model that takes a .txt file and gives the word count
+    """
+
+    def __init__(
+        self, pubsub: PubSub, config: Config, model_output_folder: Optional[Path] = None
+    ) -> None:
+        if model_output_folder:
+            self.model_output_folder = model_output_folder
+
+        super().__init__(
+            pubsub=pubsub,
+            config=config,
+            skip_moving_files=model_output_folder is None,
+        )
+
+    def run_model(self, dataset_dir: Path, output_dir: Path) -> None:
+        converted_dir = dataset_dir / "words" / "converted"
+
+        if not converted_dir.exists():
+            raise FileNotFoundError(f"Directory {converted_dir} does not exist")
+
+        txt_files: List[Path] = list(converted_dir.rglob("*.txt"))
+
+        for txt_file in txt_files:
+            word_count = self._get_word_count(txt_file)
+
+            rel_path = txt_file.relative_to(converted_dir)
+
+            output_file = output_dir / rel_path
+            output_file.parent.mkdir(parents=True, exist_ok=True)
+
+            with open(output_file, "w") as f:
+                f.write(f"Word count: {word_count}\n")
+
+    def _get_word_count(self, file: Path) -> int:
+        try:
+            with open(file, "r", encoding="utf-8") as f:
+                content = f.read()
+                words = content.split()
+                return len(words)
+        except Exception as e:
+            print(f"Error reading file {file}: {e}")
+            return 0
